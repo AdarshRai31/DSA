5ababd996fe4ad8185dd1f5b71296a9a5fdc088d
AdarshRai31 <arai89100@gmail.com>
2025-11-28 10:00:40 +0530

    Time: 486 ms (37.37%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,5 +2,5 @@
 select w1.id from
 Weather as w1
 join Weather as w2
-on date_add(w2.recordDate, interval 1 day) = w1.recordDate
+on datediff(w1.recordDate, w2.recordDate) = 1
 where w1.Temperature > w2.Temperature;
